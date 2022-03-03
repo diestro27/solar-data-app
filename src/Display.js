11c0cba5eb20470fbb96a99cd20a86cd73f77f33
diestro27 <dexterb@sol-ark.com>
2022-03-03 09:57:17 -0600

    updated colors and conditionally set the th text

--- src/Display.js
+++ src/Display.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./Display.css";
 
 function Display(props) {
+
+    const sunHoursTableHeadingText = props.peakSunHours === "" ? "Peak Sun Hours kWh/m^2/day" 
+        : "Peak Sun Hours kWh/m^2/day:";
+
     return (
         <div id={props.id}>
             <table>
                 <tbody>
                     <tr>
-                        <th>Peak Sun Hours kWh/m^2/day</th>
+                        <th>{sunHoursTableHeadingText}</th>
                         <td>{props.peakSunHours}</td>
                     </tr>
                 </tbody>
